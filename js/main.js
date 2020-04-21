1a9835a45d7dd0e3eb1b591272e1938122b39159
mixfolio <62788627+mixfolio@users.noreply.github.com>
2020-04-21 22:27:27 +0300

    Modal-message

--- js/main.js
+++ js/main.js
@@ -60,3 +60,22 @@
     };
     scrollTo();
 }());
+
+// Modal 
+
+var link = document.querySelector(".send-message");
+        var popup = document.querySelector(".modal-message");
+        var close = document.querySelector(".modal-close");
+        var login = popup.querySelector("[name=login]");
+
+        link.addEventListener("click", function (evt) {
+            evt.preventDefault();
+            popup.classList.add("modal-show");
+            login.focus();
+        });
+
+        close.addEventListener("click", function (evt) {
+            evt.preventDefault();
+            popup.classList.remove("modal-show");
+
+        });
